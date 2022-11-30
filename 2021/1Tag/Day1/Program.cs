2905dc0a4e40a2569ef1802736bc12b3a50aa345
Eren UEzer <eren.uezer@theobald-software.com>
2022-11-30 13:43:56 +0100

    Upload Day1

---
+++ 2021/1Tag/Day1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Stream stdIn = Console.OpenStandardInput();
+using StreamReader sr = new(stdIn);
+// string? line = null;
+// while ((line = sr.ReadLine()) != null)
+// {
+//     Console.WriteLine(line);
+// }
+while (!sr.EndOfStream)
+{
+    string line = sr.ReadLine();
+    System.Console.WriteLine(line);
+}
+// or
+//Stream stdOut = Console.OpenStandardOutput();
+//stdIn.CopyTo(stdOut);
+
+
+// string[] content = File.ReadAllLines("../1input.txt");
+// foreach(string l in content)
+// {
+//     System.Console.WriteLine(l);
+// }
