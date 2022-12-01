c3890aebb41926e74844e7008d9ba24ca614d82f
Eren UEzer <eren.uezer@theobald-software.com>
2022-12-01 11:28:43 +0100

    Complete first Task of Day 1 2021

--- 2021/1Tag/Day1/Program.cs
+++ 2021/1Tag/Day1/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
 Stream stdIn = Console.OpenStandardInput();
 using StreamReader sr = new(stdIn);
 // string? line = null;
@@ -7,18 +8,33 @@ using StreamReader sr = new(stdIn);
 // {
 //     Console.WriteLine(line);
 // }
-while (!sr.EndOfStream)
-{
-    string line = sr.ReadLine();
-    System.Console.WriteLine(line);
-}
+//
+//or
+//
+// while (!sr.EndOfStream)
+// {
+//     string line = sr.ReadLine();
+//     System.Console.WriteLine(line);
+// }
+//
 // or
 //Stream stdOut = Console.OpenStandardOutput();
 //stdIn.CopyTo(stdOut);
 
 
-// string[] content = File.ReadAllLines("../1input.txt");
-// foreach(string l in content)
+string[] content = File.ReadAllLines("1input.txt");
+// foreach(string line in content)
 // {
-//     System.Console.WriteLine(l);
+//     System.Console.WriteLine(line);
 // }
+
+int[] intContent = Array.ConvertAll(content, int.Parse);
+int counter = 0;
+for(int i = 1; i < intContent.Length; i++)
+{
+    if(intContent[i] > intContent[i - 1])
+    {
+        counter++;
+    }
+}
+System.Console.WriteLine(counter);
